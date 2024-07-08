a576cf50a491879ffa8b16eb3512e51d0ead290a
HimansuWebCoder <naikhimansu546@gmail.com>
2024-07-08 18:23:53 +0530

    update food-store

--- foods.js
+++ foods.js
@@ -1,19 +1,84 @@
-const fruits = [
-	"images/apple.jpeg",
-	"images/lichi.jpg",
-	"images/mango.jpeg",
-	"images/orange.jpg",
-  ];
+// const fruits = [
+// 	"images/apple.jpeg",
+// 	"images/lichi.jpg",
+// 	"images/mango.jpeg",
+// 	"images/orange.jpg",
+//   ];
+//   const veg = [
+// 	"images/brinjal.jpeg",
+// 	"images/potato.jpg",
+// 	"images/carrot.jpeg",
+// 	"images/tomato.jpeg",
+//   ];
+//   const nonVeg = [
+// 	"images/egg.jpeg",
+// 	"images/mutton.jpeg",
+// 	"images/chicken.jpg",
+//   ];
+
+  const fruits = [
+  	     {
+  	     	name: "Apple",
+  	     	src: "images/apple.jpeg",
+  	     	alt: "Apple"
+  	     },
+         {
+  	     	name: "Lichi",
+  	     	src: "images/lichi.jpg",
+  	     	alt: "Lichi"
+  	     },
+         {
+  	     	name: "Mango",
+  	     	src: "images/mango.jpeg",
+  	     	alt: "Mango"
+  	     },
+         {
+  	     	name: "Orange",
+  	     	src: "images/orange.jpg",
+  	     	alt: "Orange"
+  	     }
+  ];   
+
   const veg = [
-	"images/brinjal.jpeg",
-	"images/potato.jpg",
-	"images/carrot.jpeg",
-	"images/tomato.jpeg",
+  	     {
+  	     	name: "Brinjal",
+  	     	src: "images/brinjal.jpeg",
+  	     	alt: "Brinjal"
+  	     },
+         {
+  	     	name: "Potato",
+  	     	src: "images/potato.jpg",
+  	     	alt: "Potato"
+  	     },
+         {
+  	     	name: "Carrot",
+  	     	src: "images/carrot.jpeg",
+  	     	alt: "Carrot"
+  	     },
+         {
+  	     	name: "Tomato",
+  	     	src: "images/tomato.jpeg",
+  	     	alt: "Tomato"
+  	     }
   ];
+
   const nonVeg = [
-	"images/egg.jpeg",
-	"images/mutton.jpeg",
-	"images/chicken.jpg",
+  	     {
+  	     	name: "Egg",
+  	     	src: "images/egg.jpeg",
+  	     	alt: "Egg"
+  	     },
+         {
+  	     	name: "Mutton",
+  	     	src: "images/mutton.jpeg",
+  	     	alt: "Mutton"
+  	     },
+         {
+  	     	name: "Chicken",
+  	     	src: "images/chicken.jpg",
+  	     	alt: "Chicken"
+  	     }
   ];
+  
 
   export { fruits, veg, nonVeg};
--- script.js
+++ script.js
@@ -1,48 +1,77 @@
 const btns = document.querySelectorAll("button");
 const foodContainer = document.querySelector("#output");
+const searchInput = document.querySelector("#food-search");
+const errorShowBox = document.querySelector("#error-box");
+const errorHeaderMsg = document.querySelector("#error-header");
 
 import { fruits, veg, nonVeg} from "./foods.js"
 const allItems = [...fruits, ...veg, ...nonVeg];
 
+
+function createImage(img, src, name, style) {
+	const image = document.createElement(img);
+	foodContainer.appendChild(image);
+	image.setAttribute("alt", name);
+	image.setAttribute("src", src);
+	image.setAttribute("id", "images")
+	image.classList.add(style);
+}
+
+
+// Refactoring creating images DRY run
 allItems.forEach((x) => {
-  const image = document.createElement("img");
-  foodContainer.appendChild(image);
-  image.setAttribute("src", x);
-  image.setAttribute("alt", x);
-  image.classList.add("fruits-style");
+  // const image = document.createElement("img");
+  // foodContainer.appendChild(image);
+  // image.setAttribute("src", x);
+  // image.setAttribute("alt", x);
+  // image.classList.add("fruits-style");
+  createImage("img", x.src, x.alt, "fruits-style")
 });
+
+const images = document.querySelectorAll("#images");
+
 
 btns.forEach((btn) => {
   btn.addEventListener("click", (e) => {
 	foodContainer.textContent = "";
 	if (e.target.textContent === "Fruits") {
 	  fruits.forEach((x, i) => {
-		const image = document.createElement("img");
-		foodContainer.appendChild(image);
-		image.setAttribute("src", x);
-		image.classList.add("fruits-style");
+	  	  createImage("img", x.src, x.alt, "fruits-style")
 	  });
 	} else if (e.target.textContent === "Veg") {
 	  veg.forEach((x, i) => {
-		const image = document.createElement("img");
-		foodContainer.appendChild(image);
-		image.setAttribute("src", x);
-		image.classList.add("fruits-style");
+	  	  createImage("img", x.src, x.alt, "fruits-style")
 	  });
 	} else if (e.target.textContent === "Non-Veg") {
 	  nonVeg.forEach((x, i) => {
-		const image = document.createElement("img");
-		foodContainer.appendChild(image);
-		image.setAttribute("src", x);
-		image.classList.add("fruits-style");
+	  	  createImage("img", x.src, x.alt, "fruits-style")
 	  });
 	} else {
-	  allItems.forEach((x) => {
-		const image = document.createElement("img");
-		foodContainer.appendChild(image);
-		image.setAttribute("src", x);
-		image.classList.add("fruits-style");
+	  allItems.forEach((x, i) => {
+	  	  createImage("img", x.src, x.alt, "fruits-style")
 	  });
 	}
   });
 });
+
+
+
+searchInput.addEventListener("input", () => {
+	   let isSearched = false; 
+		allItems.forEach((x, i) => {
+			if (x.name.toUpperCase().includes(searchInput.value.toUpperCase())) {
+        images[i].style.display = "block";
+				isSearched = true;
+				errorHeaderMsg.textContent = ""
+				errorShowBox.style.display = ""
+			} else {
+				images[i].style.display = "none";
+			}
+		})
+
+		if (!isSearched) {
+			console.log("not found");
+			errorHeaderMsg.textContent = "Food Not Found Please search again";
+			errorShowBox.style.display = "block";
+		}
+})
